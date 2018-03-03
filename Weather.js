8a4259640a7e628df7bc7ad0e49c577a1bc5620c
Jinhyunly <dbdbddipp@gmail.com>
2018-03-04 02:50:27 +0900

    modify content

--- Weather.js
+++ Weather.js
@@ -8,49 +8,49 @@ const weatherCases = {
     Rain: {
         colors:["#00C6FB","#005BEA"],
         title: "Raining like a MF",
-        subtitle: "For more info look outside",
+        subtitle: "hello Rain",
         icon: "weather-rainy"
     },
     Clear:{
         colors:["#FEF253","#FF7300"],
         title: "Sunny as a MF",
-        subtitle: "Go get your asss burnt",
+        subtitle: "hello Sunny",
         icon: "weather-sunny"
     },
     Thunderstorm:{
         colors:["#00ECBC","#007ADF"],
         title: "Thunderstorm in the house",
-        subtitle: "Actually, outside if the house",
+        subtitle: "hello Thunderstorm",
         icon: "weather-lightning"
     },
     Clouds:{
         colors: ["#D7D2CC", "#304352"],
         title: "Clouds",
-        subtitle: "I know, fucking boring",
+        subtitle: "hello Cloud",
         icon: "weather-cloudy"
     },
     Snow:{
         colors: ["#7DE2FC", "#B9B6E5"],
         title: "Cold as balls",
-        subtitle: "Do you want to build a snowman? fuck no",
+        subtitle: "hello Snow",
         icon: "weather-snowy"
     },
     Drizzle:{
         colors: ["#89F7FE", "#66A6FF"],
         title: "Drizzle",
-        subtitle: "Is like rain, but gay",
+        subtitle: "hello Drizzle",
         icon: "weather-hail"
     },
     Haze: {
         colors: ["#89F7FE", "#66A6FF"],
         title: "Haze",
-        subtitle: "Don't know what that is 💩",
+        subtitle: "hello Haze",
         icon: "weather-hail"
       },
     Mist: {
         colors: ["#D7D2CC", "#304352"],
         title: "Mist!",
-        subtitle: "It's like you have no glasses on.",
+        subtitle: "hello Mist",
         icon: "weather-fog"
     }
 };
